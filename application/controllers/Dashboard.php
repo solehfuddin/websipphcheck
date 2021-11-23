dafb56282884816a2c17937951372b4c5002e58f
solehfuddin <solehfuddin@gmail.com>
2021-11-23 08:19:57 +0700

    Penyesuaian dimensi gambar

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -36,9 +36,9 @@ class Dashboard extends CI_Controller {
 
 		$config['upload_path']          = './assets/img/';
 		$config['allowed_types']        = 'gif|jpg|png';
-		$config['max_size']             = 100;
-		$config['max_width']            = 1024;
-		$config['max_height']           = 768;
+		$config['max_size']             = 10000;
+		$config['max_width']            = 16000;
+		$config['max_height']           = 30000;
  
 		$this->load->library('upload', $config);
 
